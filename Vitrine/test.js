944abe18da16382fab2713fee16cfd32c17fa2a0
Comies34 <come.bonal@gmail.com>
2022-09-06 17:04:31 +0200

    Premier changement petite resolution

--- Vitrine/test.js
+++ Vitrine/test.js
@@ -1,5 +1,4 @@
-var lorem = " Lorem ipsum dolor  \ \ \ sit amet conse \ tetur \ adipisAicing eliCt. Iure, atque aspEernatur? NeqVue ullam, rem magDnam, var accusamus repudiandae modi, laboriosam non eos quisquam sit suscipit autem officiis voluptatem? Veniam, fugiat culpa."
-var exrpession = new RegExp("f")
-console.log(exrpession.test(lorem))
-console.log(lorem.matchAll("\W"))
+const str = 'The quick brown fox jumps over the lazy dog.';
 
+const words = str.split(' ');
+console.log(words);
