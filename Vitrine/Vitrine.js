5b831b7fe46c71f5bc66a1269a20909580617b19
Comies34 <come.bonal@gmail.com>
2023-02-23 10:25:07 -0800

    Setting image URL locally rather than online

--- Vitrine/Vitrine.js
+++ Vitrine/Vitrine.js
@@ -9,13 +9,13 @@ let tableauImage = [];
 let tableauImage2 = []
 let index = 0;
 let image = document.getElementById('back');
-var url = ["https://www.audi.fr/content/dam/nemo/fr/Service/Garantie/LPGarantie/HEADER_1920X600v2.jpg?imwidth=1920", "Garantie & Extension de garantie"];
-var url2 = ["https://www.audi.fr/content/dam/nemo/fr/RubriqueElectrique/Gamme/1920x600_q4_ed1_2021_V2.jpg?imwidth=1920", "La gamme 100% électrique définit la mobilité de demain"];
-var url3 = ["https://www.audi.fr/content/dam/nemo/fr/Entreprises/2021/refonte_aout_2021/header_Fleet/1920x600_Audi_Fleet_header_Audipros_04112021.jpg?imwidth=1920", "Audi pour les professionels"];
+var url = ["../img/back_bleu.avif", "Garantie & Extension de garantie"];
+var url2 = ["../img/e-trons.webp", "La gamme 100% électrique définit la mobilité de demain"];
+var url3 = ["../img/gris&noir.jpg", "Audi pour les professionels"];
 tableauImage.push(url, url2, url3)
-var url11 = ["https://www.audi.fr/content/dam/nemo/fr/Service/Garantie/LPGarantie/HEADER_1920X1080.jpg?imwidth=767", "Garantie & Extension de garantie"];
-var url22 = ["https://www.audi.fr/content/dam/nemo/fr/RubriqueElectrique/Gamme/1920x1080_q4_ed1_2021_V2.jpg?imwidth=767", "La gamme 100% électrique définit la mobilité de demain"];
-var url33 = ["https://www.audi.fr/content/dam/nemo/fr/Entreprises/2021/refonte_aout_2021/header_Fleet/1920x1080_Audi_Fleet_header_Audipros_09082021.jpg?imwidth=767", "Audi pour les professionels"];
+var url11 = ["../img/back_bleu_resp.avif", "Garantie & Extension de garantie"];
+var url22 = ["../img/e-trons_resp.webp", "La gamme 100% électrique définit la mobilité de demain"];
+var url33 = ["../img/gris&noir_resp.avif", "Audi pour les professionels"];
 tableauImage2.push(url11, url22, url33)
 let randTab = null
 console.log(document.documentElement.clientWidth)
@@ -149,7 +149,7 @@ function commence() {
     let rand = 0
     timer = setInterval(function () {
         document.documentElement.clientWidth.toFixed() > 750 ?
-        randTab = tableauImage : randTab = tableauImage2
+            randTab = tableauImage : randTab = tableauImage2
         // Le bout de code sert à faire en sorte que si l'on clique pour déplacer une image, le déplacement automatique d'image s'anulle pour les 3 prochaines secondes
         document.getElementById('button').onclick = function () {
             move(-1, randTab)
@@ -161,7 +161,7 @@ function commence() {
                 clearInterval(timer);
                 return commence();
             }
-            move(1, randTab)
+        move(1, randTab)
 
     }, 3000);
 }
