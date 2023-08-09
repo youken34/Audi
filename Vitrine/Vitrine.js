9539611346a86a5dc7238e189d0b36cd50890e73
Comies34 <come.bonal@gmail.com>
2023-08-09 19:57:03 +0200

    design massive improvements

--- Vitrine/Vitrine.js
+++ Vitrine/Vitrine.js
@@ -12,30 +12,32 @@ var url = ["../img/back_bleu.avif", "Garantie & Extension de garantie"];
 var url2 = ["../img/e-trons.webp", "La gamme 100% électrique définit la mobilité de demain"];
 var url3 = ["../img/gris&noir.jpg", "Audi pour les professionels"];
 tableauImage.push(url, url2, url3)
-var url11 = ["../img/back_bleu_resp.avif", "Garantie & Extension de garantie"];
-var url22 = ["../img/e-trons_resp.webp", "La gamme 100% électrique définit la mobilité de demain"];
-var url33 = ["../img/gris&noir_resp.avif", "Audi pour les professionels"];
+var url11 = ["../img/a-pleine-vitesse.avif", "À pleine vitesse électrique", "Audi a récemment étayé son parcours clair vers l'électromobilité avec des chiffres forts et des jalons importants. Comment les Quatre Anneaux progressent sur leur chemin vers une électrification réussie."];
+var url22 = ["../img/travail-en-cours.avif", "#Travail en cours", "Aidez-nous à concevoir la mobilité de demain. Avec des voitures qui utilisent une technologie de pointe pour offrir une expérience de conduite exceptionnelle et, en même temps, ouvrir la voie à un avenir numérique, durable et électrique."];
+var url33 = ["../img/e-trons_resp.webp", "Chez Audi, vous trouverez le bon véhicule pour chaque besoin", "Sportif, luxueux ou compact – chez Audi, vous trouverez le véhicule adapté à chaque exigence. Découvrez et configurez dès maintenant nos modèles de série."];
 tableauImage2.push(url11, url22, url33)
 let randTab = null
-document.getElementById('output').innerHTML = tableauImage[index][1];
+document.getElementById('output').innerHTML = tableauImage2[index][1];
+document.getElementById('description').innerHTML = tableauImage2[index][2];
+
 window.onload = function () {
     commence();
 };
 
 let menu = true
-window.addEventListener('resize', function () {
-    let randTab = []
-    setInterval(function () {
-        if (document.documentElement.clientWidth.toFixed() < 500) {
-            randTab = tableauImage2
+// window.addEventListener('resize', function () {
+//     let randTab = []
 
-        } else if (document.documentElement.clientWidth.toFixed() > 500) {
-            randTab = tableauImage
-        }
-        image.style.backgroundImage = "url(" + randTab[index][0] + ")"
+//     if (document.documentElement.clientWidth.toFixed() < 500) {
+//         randTab = tableauImage2
 
-    }, 100)
-})
+//     } else if (document.documentElement.clientWidth.toFixed() > 500) {
+//         randTab = tableauImage
+//     }
+//     image.style.backgroundImage = "url(" + randTab[index][0] + ")"
+
+
+// })
 
 
 window.addEventListener('resize', function () {
@@ -125,19 +127,22 @@ function move(params, tableauImage) {
     if (tableauImage[index + params]) {
         index = index + params
         image.style.transition = "1s";
-        image.style.backgroundImage = "url(" + tableauImage[index][0] + ")";
+        image.src = tableauImage[index][0]
         document.getElementById('output').innerHTML = tableauImage[index][1];
+        document.getElementById('description').innerHTML = tableauImage2[index][2];
 
     }
     else if (params > 0) {
         index = 0;
-        image.style.backgroundImage = "url(" + tableauImage[index][0] + ")";
+        image.src = tableauImage[index][0]
         document.getElementById('output').innerHTML = tableauImage[index][1];
+        document.getElementById('description').innerHTML = tableauImage2[index][2];
     }
     else {
         index = tableauImage.length - 1;
-        image.style.backgroundImage = "url(" + tableauImage[index][0] + ")";
+        image.src = tableauImage[index][0]
         document.getElementById('output').innerHTML = tableauImage[index][1];
+        document.getElementById('description').innerHTML = tableauImage2[index][2];
     }
 }
 function getRandomInt(min, max) {
@@ -146,20 +151,10 @@ function getRandomInt(min, max) {
     console.log(Math.ceil(Math.random() * (max - min + 1)) + min - 1);
 }
 function commence() {
-    let randTab = []
+    let randTab = tableauImage2
+    image.src = randTab[index][0]
 
-    if (document.documentElement.clientWidth.toFixed() < 500) {
-        randTab = tableauImage2
-
-    } else if (document.documentElement.clientWidth.toFixed() > 500) {
-        randTab = tableauImage
-    }
-    image.style.backgroundImage = "url(" + randTab[index][0] + ")"
-
-    let rand = 0
     timer = setInterval(function () {
-        document.documentElement.clientWidth.toFixed() > 750 ?
-            randTab = tableauImage : randTab = tableauImage2
         // Le bout de code sert à faire en sorte que si l'on clique pour déplacer une image, le déplacement automatique d'image s'anulle pour les 3 prochaines secondes
         document.getElementById('button').onclick = function () {
             move(-1, randTab)
@@ -173,7 +168,7 @@ function commence() {
             }
         move(1, randTab)
 
-    }, 3000);
+    }, 5000);
 }
 
 window.addEventListener('scroll', () => {
@@ -185,56 +180,54 @@ window.addEventListener('scroll', () => {
     // getBoundingClientRect() est un objet qui stock toute les propriétés relative à la position d'une balise
     // Ici on spécifie vouloir la valeur 'top' qui indique la distance (en px) entre le haut de notre élément et le haut du client
     if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8) {
-        document.getElementsByClassName('monAudi')[0].style.transform = 'translateY(0px)'
         message.classList.add('active')
     }
     else {
         message.classList.remove('active')
-        document.getElementsByClassName('monAudi')[0].style.transform = 'translateY(-45px)'
     }
 })
 
 
-function getBackgroundImageHeight(containerElement) {
-    const backgroundImage = new Image();
-    backgroundImage.src = getComputedStyle(containerElement).backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/, '$1');
+// function getBackgroundImageHeight(containerElement) {
+//     const backgroundImage = new Image();
+//     backgroundImage.src = getComputedStyle(containerElement).backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/, '$1');
 
-    return new Promise(resolve => {
-        backgroundImage.onload = function () {
-            const containerWidth = containerElement.clientWidth;
-            const imageWidth = backgroundImage.width;
-            const imageHeight = backgroundImage.height;
+//     return new Promise(resolve => {
+//         backgroundImage.onload = function () {
+//             const containerWidth = containerElement.clientWidth;
+//             const imageWidth = backgroundImage.width;
+//             const imageHeight = backgroundImage.height;
 
-            const scaledImageHeight = (containerWidth / imageWidth) * imageHeight;
-            resolve(scaledImageHeight);
-        };
-    });
-}
+//             const scaledImageHeight = (containerWidth / imageWidth) * imageHeight;
+//             resolve(scaledImageHeight);
+//         };
+//     });
+// }
 
 // Usage
-window.addEventListener('resize', function () {
-    const containerElement = document.getElementById('back');
-    getBackgroundImageHeight(containerElement).then(height => {
-        document.getElementById('back').style.height = `${height + 1}px`
+// window.addEventListener('resize', function () {
+//     const containerElement = document.getElementById('back');
+//     getBackgroundImageHeight(containerElement).then(height => {
+//         document.getElementById('back').style.height = `${height + 1}px`
 
-    });
-})
-window.addEventListener('load', function () {
-    const containerElement = document.getElementById('back');
-    getBackgroundImageHeight(containerElement).then(height => {
-        document.getElementById('back').style.height = `${height - 1}px`
+//     });
+// })
+// window.addEventListener('load', function () {
+//     const containerElement = document.getElementById('back');
+//     getBackgroundImageHeight(containerElement).then(height => {
+//         document.getElementById('back').style.height = `${height - 1}px`
 
-    });
-})
+//     });
+// })
 
 
-setInterval(function () {
-    const containerElement = document.getElementById('back');
-    getBackgroundImageHeight(containerElement).then(height => {
-        document.getElementById('back').style.height = `${height + 2}px`
-    });
+// setInterval(function () {
+//     const containerElement = document.getElementById('back');
+//     getBackgroundImageHeight(containerElement).then(height => {
+//         document.getElementById('back').style.height = `${height + 2}px`
+//     });
 
-}, 100)
+// }, 100)
 
 
 
