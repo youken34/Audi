f7b8a44c2ceeb927658ef4f78dcaddcba7792524
Comies34 <come.bonal@gmail.com>
2023-09-12 23:34:29 +0200

    multiple improvements

--- Inscription/inscription.js
+++ Inscription/inscription.js
@@ -1,92 +1,87 @@
-let container = document.querySelector(".container")
-const formulaire = document.querySelector('.formulaire');
+let container = document.querySelector(".container");
+const formulaire = document.querySelector(".formulaire");
 let menu = true;
-const boutonMenu = document.querySelector('#symbole')
-const banner = document.querySelector('.banner')
-const link = document.querySelector('#link')
-const liens = document.querySelectorAll('#link a')
-console.log(document.documentElement.clientWidth)
-function demarrer() {
-    if (container.getBoundingClientRect().top.toFixed() < 300) {
-        container.classList.add('animContainer')
-        formulaire.style.transitionDelay = "0.8s"
-        setTimeout(formulaire.classList.add('anim')); 
+const boutonMenu = document.querySelector("#symbole");
+const banner = document.querySelector(".banner");
+const link = document.querySelector("#link");
+const liens = document.querySelectorAll("#link a");
 
+window.addEventListener("resize", function () {
+  if (document.documentElement.clientWidth.toFixed() > 750) {
+    banner.style.height = "80px";
+    menu = true;
+    document.body.style.overflowY = "visible";
+    boutonMenu.className = "fas fa-align-justify";
+    var index = 0,
+      length = liens.length;
+    for (; index < length; index++) {
+      liens[index].style.display = "block";
     }
-    else{
-        container.classList.remove('animContainer')
-        setTimeout(formulaire.classList.add('anim')); 
-
+  } else if (document.documentElement.clientWidth.toFixed() < 750) {
+    if (menu === true) {
+      var index = 0,
+        length = liens.length;
+      for (; index < length; index++) {
+        liens[index].style.display = "none";
+      }
     }
-}
-
-
-window.addEventListener('resize', function () {
-
-
-    if (document.documentElement.clientWidth.toFixed() > 750) {
-        banner.style.height = "100px";
-        menu = true
-        document.body.style.overflowY = "visible"
-        boutonMenu.className = "fas fa-align-justify"
-        var index = 0, length = liens.length;
-        for (; index < length; index++) {
-            liens[index].style.display = "block"
-        }
-
-
-    }
-    else if (document.documentElement.clientWidth.toFixed() < 750) {
-        if (menu === true) {
-            var index = 0, length = liens.length;
-            for (; index < length; index++) {
-                liens[index].style.display = "none";
-            }
-        }
-
-
-    }
-
-
-})
+  }
+});
 
 function MenuDepliant() {
-    menu = !menu
-    if (menu === true) {
-        boutonMenu.className = "fas fa-align-justify"
-        boutonMenu.className.remove = "close icon"
-        document.body.style.height = "200vh";
-        banner.style.height = "100px";
-        document.body.style.overflowY = "visible"
-        link.classList.remove('linkResponsive')
-        var index = 0, length = liens.length;
-        for (; index < length; index++) {
-            liens[index].style.display = "none";
-        }
+  menu = !menu;
+  if (menu === true) {
+    boutonMenu.className = "fas fa-align-justify";
+    boutonMenu.className.remove = "close icon";
+    document.body.style.height = "200vh";
+    banner.style.height = "80px";
+    document.body.style.overflowY = "visible";
+    link.classList.remove("linkResponsive");
+    var index = 0,
+      length = liens.length;
+    for (; index < length; index++) {
+      liens[index].style.display = "none";
     }
-    else {
-        boutonMenu.className = "close icon"
-        boutonMenu.className.remove = "fas fa-align-justify"
-        document.body.style.height = "100vh";
-        banner.style.height = "100%";
-        document.body.style.overflowY = "hidden"
-        link.classList.add('linkResponsive') // S'additionne seulement, ne modifie pas les propriétés
-        var index = 0, length = liens.length;
-        for (; index < length; index++) {
-            liens[index].style.display = "block";
-        }
+  } else {
+    boutonMenu.className = "close icon";
+    boutonMenu.className.remove = "fas fa-align-justify";
+    document.body.style.height = "100vh";
+    banner.style.height = "100%";
+    document.body.style.overflowY = "hidden";
+    link.classList.add("linkResponsive"); // S'additionne seulement, ne modifie pas les propriétés
+    var index = 0,
+      length = liens.length;
+    for (; index < length; index++) {
+      liens[index].style.display = "block";
     }
-
+  }
 }
 
-const navigation = document.querySelector('nav');
+const navigation = document.querySelector("nav");
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.scrollY > 200) {
+    navigation.style.setProperty(
+      "--banner-before-opacity",
+      "rgba(255, 255, 255, 1)"
+    );
+  } else {
+    navigation.style.setProperty(
+      "--banner-before-opacity",
+      "rgba(255, 255, 255, 0.5)"
+    );
+  }
+});
 
-window.addEventListener('scroll', () => {
-
-    if (window.scrollY > 100) {
-        navigation.classList.add('anim-nav');
-    } else {
-        navigation.classList.remove('anim-nav');
-
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    navigation.style.setProperty(
+      "--banner-before-opacity",
+      "rgba(255, 255, 255, 1)"
+    );
+  } else {
+    navigation.style.setProperty(
+      "--banner-before-opacity",
+      "rgba(255, 255, 255, 0.5)"
+    );
+  }
+});
---
+++ Inscription/three.js
@@ -0,0 +1,102 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/DRACOLoader.js";
+import { RGBELoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/RGBELoader.js";
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath(
+  "https://www.gstatic.com/draco/versioned/decoders/1.5.6/"
+);
+dracoLoader.preload();
+
+const loadingManager = new THREE.LoadingManager();
+
+const progressBar = document.getElementById("progress-bar");
+
+loadingManager.onProgress = function (url, loaded, total) {
+  progressBar.value = (loaded / total) * 100;
+};
+
+const progressBarContainer = document.querySelector(".progress-bar-container");
+// loadingManager.onLoad = function () {
+//   progressBarContainer.style.display = "none";
+// };
+
+const loader = new GLTFLoader(loadingManager);
+
+loader.setDRACOLoader(dracoLoader);
+
+const mainScene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, 2.56, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth - 350, 600);
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+document.getElementById("threeModel").appendChild(renderer.domElement);
+
+let gltfObject; // Declare a variable to hold the loaded GLTF object
+let rgbeLoader = new RGBELoader();
+rgbeLoader.load(
+  "../texture/MR_INT-002_BathroomHard_Pierre.hdr",
+  function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    mainScene.environment = texture;
+    loader.load(
+      "../glb/audi_rs5.glb",
+      function (gltf) {
+        gltfObject = gltf.scene; // Store the loaded object for later access
+        gltfObject.position.set(0, 0, 0);
+
+        mainScene.add(gltfObject);
+
+        camera.position.set(
+          720.7022656180474,
+          64.84397893460088,
+          -293.87523289241403
+        );
+        camera.lookAt(gltfObject.position);
+
+        //   Add ambient light
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+        mainScene.add(ambientLight);
+
+        // Add directional light
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(-1, 2, 1);
+        directionalLight.intensity = 1;
+        mainScene.add(directionalLight);
+      },
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "loaded");
+      },
+      function (error) {
+        console.log(error);
+      }
+      // Progress and error handlers...
+    );
+  }
+);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = false;
+controls.dampingFactor = 0.05;
+controls.enableZoom = false; // Disable zoom
+controls.enablePan = false; // Disable panning
+controls.enableRotate = false; // Disable rotation
+
+const rotateSpeed = 0.005; // Adjust the rotation speed as needed
+
+const animate = () => {
+  if (gltfObject) {
+    gltfObject.rotation.y += rotateSpeed; // Rotate the loaded object
+  }
+
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(mainScene, camera);
+};
+
+animate();
+renderer.setClearColor(0xa3a3a3);
